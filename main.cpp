42eaa3804e8b3091eb13ff7ddfec20314c840895
Rinsuki <25454024+rinsukiyo@users.noreply.github.com>
2021-02-28 03:39:52 +0800

    Hiragana additional sounds
    Now two versions of the hiragana practice system available

--- main.cpp
+++ main.cpp
@@ -89,38 +89,37 @@ int main(int argc, const char * argv[]) {
     };
     
     // Welcome
-//    cout << "Welcome! This is a software that allows you to review hiragana." << endl;
-//    sleep(3);
-//    cout << "If you haven't finished learning it yet, you may encounter some hiragana that you don't know during the use of this software." << endl;
-//    sleep(5);
+    cout << "Welcome! This is a software that allows you to review hiragana." << endl;
+    sleep(3);
+    cout << "If you haven't finished learning it yet, you may encounter some hiragana that you don't know during the use of this software." << endl;
+    sleep(5);
     
     // Beginning
-//    while (input != "1" || input != "2") {
     string input = "1";
-    cout << "Which part do you want to practice?" << endl;
-    cout << "1 > Hiragana" << endl;
-    cout << "2 > Katakana" << endl;
-    cin >> input;
-//    }
-    if (input == "1") {
-        cout << "Do you want the hiragana WITH or WITHOUT the turbinate included?" << endl;
+//    cout << "Which part do you want to practice?" << endl;
+//    cout << "1 > Hiragana" << endl;
+//    cout << "2 > Katakana" << endl;
+//    cin >> input;
+//    if (input == "1") {
+        cout << "Do you want the hiragana WITH or WITHOUT the additional sounds included? Such as ば, ぴ." << endl;
         cout << "1 > Yes (WITH)" << endl;
         cout << "2 > No (WITHOUT)" << endl;
         cin >> input;
-        }
+//        }
+    // Full Version of Hiragana
         if (input == "1") {
             for (;input != "exit";) {
                 int number = rand() % (46 + 25);
                 auto questionString = to_string(question);
                 auto correctAnswersString = to_string(correctAnswers);
                 
-//                // Debug Mode
-//                cout << "\n<<< Debug Mode >>>" << endl;
-//                cout << "Question Number: " + questionString << endl;
-//                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
-//                cout << question << endl;
-//                cout << correctAnswers << endl;
-//                // Debug Mode End
+        //                // Debug Mode
+        //                cout << "\n<<< Debug Mode >>>" << endl;
+        //                cout << "Question Number: " + questionString << endl;
+        //                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
+        //                cout << question << endl;
+        //                cout << correctAnswers << endl;
+        //                // Debug Mode End
                 
                 // Question
                 cout << "Question " + questionString + ": " << endl;
@@ -138,13 +137,46 @@ int main(int argc, const char * argv[]) {
                 // Question End
             }
             
-        } else if (input == "2") {
-//            number = rand() % (46);
         }
+        // Easy Version of Hiragana
+        else if (input == "2") {
+            for (;input != "exit";) {
+                int number = rand() % (46);
+                auto questionString = to_string(question);
+                auto correctAnswersString = to_string(correctAnswers);
+                
+        //                // Debug Mode
+        //                cout << "\n<<< Debug Mode >>>" << endl;
+        //                cout << "Question Number: " + questionString << endl;
+        //                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
+        //                cout << question << endl;
+        //                cout << correctAnswers << endl;
+        //                // Debug Mode End
+                
+                // Question
+                cout << "Question " + questionString + ": " << endl;
+                cout << "How should " + hiragana[number] + " be pronounced?" << endl;
+                question++;
+                cin >> input;
+                if (input == hiraganaAnswer[number]) {
+                    cout << "Congratulations! Your answer is correct!" << endl;
+                    correctAnswers++;
+                    
+                } else {
+                    cout << "Keep practicing! The pronunciation of " + hiragana[number] + " is " + hiraganaAnswer[number] + "!" << endl;
+                    mistakes++;
+                }
+                // Question End
+            }
+            // Easy Version of Hiragana End
+    
     // Over
     auto questionString = to_string(question - 2);
     auto correctAnswersString = to_string(correctAnswers);
-    cout << "Thank you for your hard work! You answered a total of " + questionString + " questions, of which " + correctAnswersString + " were correct." << endl;
+            cout << "Thank you for your hard work! You answered a total of " + questionString + " questions, of which " + correctAnswersString + " were correct." << endl;
     return 0;
-    }
+                
+            
+        }
     
+}
