0ab515d8225ceae4c3b5214a513eec626a480b40
Rinsuki <25454024+rinsukiyo@users.noreply.github.com>
2021-02-28 19:42:12 +0800

    Incomplete error analysis system (hidden)
    A number of unexpected errors have occurred.
    1. An execution error occurred in the program when push_back was executed.
    2. The last element of push_back always overwrites all other elements.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 #include <unistd.h>
 #include <vector>
 #include <stdlib.h>
+#include <algorithm>
+// #include <iterator> // begin(), end()
+
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -17,6 +20,8 @@ int main(int argc, const char * argv[]) {
     int question = 1;
     int correctAnswers = 0;
     int mistakes = 0;
+    //    string mistakesArray [] = {};
+    //    string unskilled[] = {};
     
     // Dictionary
     string hiragana[] = {
@@ -101,46 +106,60 @@ int main(int argc, const char * argv[]) {
     
     // Beginning
     string input = "1";
-//    cout << "Which part do you want to practice?" << endl;
-//    cout << "1 > Hiragana" << endl;
-//    cout << "2 > Katakana" << endl;
-//    cin >> input;
-//    if (input == "1") {
+    cout << "Which part do you want to practice?" << endl;
+    cout << "1 > Hiragana" << endl;
+    cout << "2 > Katakana" << endl;
+    cin >> input;
+    if (input == "1") {
         cout << "Do you want the hiragana WITH or WITHOUT the additional sounds included? Such as ば, ぴ." << endl;
         cout << "1 > Yes (WITH)" << endl;
         cout << "2 > No (WITHOUT)" << endl;
         cin >> input;
-//        }
-    
-    system("clear");
-    
-    // Full Version of Hiragana
+        //        }
+        
+        system("clear");
+        
+        // Full Version of Hiragana
         if (input == "1") {
             for (;input != "stop";) {
                 int number = rand() % (46 + 25);
                 auto questionString = to_string(question);
                 auto correctAnswersString = to_string(correctAnswers);
                 
-        //                // Debug Mode
-        //                cout << "\n<<< Debug Mode >>>" << endl;
-        //                cout << "Question Number: " + questionString << endl;
-        //                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
-        //                cout << question << endl;
-        //                cout << correctAnswers << endl;
-        //                // Debug Mode End
+                // Debug Mode
+                //                cout << "\n<<< Debug Mode >>>" << endl;
+                //                cout << "Question Number: " + questionString << endl;
+                //                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
+                //                cout << question << endl;
+                //                cout << correctAnswers << endl;
+                //                cout << mistakesArray[0] << endl;
+                // Debug Mode End
                 
                 // Question
+                string tempHiragana = hiragana[number];
                 cout << "Question " + questionString + ": " << endl;
-                cout << "How should " + hiragana[number] + " be pronounced?" << endl;
+                cout << "How should " + tempHiragana + " be pronounced?" << endl;
                 question++;
                 cin >> input;
+                //                vector<string> mistakesArray;
+                //                vector<string> unskilled;
                 if (input == hiraganaAnswer[number]) {
                     cout << "Congratulations! Your answer is correct!\n" << endl;
                     correctAnswers++;
                     
+                    // Wrong Answer
                 } else {
-                    cout << "Keep practicing! The pronunciation of " + hiragana[number] + " is " + hiraganaAnswer[number] + "!\n" << endl;
+                    cout << "Keep practicing! The pronunciation of " + tempHiragana + " is " + hiraganaAnswer[number] + "!\n" << endl;
                     mistakes++;
+                    // Categorising Mistakes (1/2+)
+                    //                    if (binary_search(mistakesArray.begin(), mistakesArray.end(), tempHiragana)) {
+                    //                        mistakesArray.push_back(tempHiragana);
+                    //                    } else {
+                    //                        unskilled.push_back(tempHiragana);
+                    //                    }
+                    //                    // Debug Mode
+                    //                    cout << tempHiragana << endl;
+                    //                    cout << mistakesArray[0] << endl;
                 }
                 // Question End
             }
@@ -153,36 +172,70 @@ int main(int argc, const char * argv[]) {
                 auto questionString = to_string(question);
                 auto correctAnswersString = to_string(correctAnswers);
                 
-        //                // Debug Mode
-        //                cout << "\n<<< Debug Mode >>>" << endl;
-        //                cout << "Question Number: " + questionString << endl;
-        //                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
-        //                cout << question << endl;
-        //                cout << correctAnswers << endl;
-        //                // Debug Mode End
+                // Debug Mode
+                //                cout << "\n<<< Debug Mode >>>" << endl;
+                //                cout << "Question Number: " + questionString << endl;
+                //                cout << "Correct Answers: " + correctAnswersString + "\n" << endl;
+                //                cout << question << endl;
+                //                cout << correctAnswers << endl;
+                //                cout << mistakesArray[0] << endl;
+                // Debug Mode End
                 
                 // Question
+                string tempHiragana = hiragana[number];
                 cout << "Question " + questionString + ": " << endl;
-                cout << "How should " + hiragana[number] + " be pronounced?" << endl;
+                cout << "How should " + tempHiragana + " be pronounced?" << endl;
                 question++;
                 cin >> input;
                 if (input == hiraganaAnswer[number]) {
                     cout << "Congratulations! Your answer is correct!\n" << endl;
                     correctAnswers++;
                     
+                    // Wrong Answer
                 } else {
                     cout << "Keep practicing! The pronunciation of " + hiragana[number] + " is " + hiraganaAnswer[number] + "!\n" << endl;
                     mistakes++;
+                    //                    vector<string> mistakesArray;
+                    //                    vector<string> unskilled;
+                    //                    // Categorising Mistakes (1/2+)
+                    //                    if (find(begin(mistakesArray), end(mistakesArray), tempHiragana) != mistakesArray.end()) {
+                    //                        mistakesArray.push_back(tempHiragana);
+                    //                    } else {
+                    //                        unskilled.push_back(tempHiragana);
+                    //                    }
+                    //                    // Debug Mode
+                    //                    cout << mistakesArray[0] << endl;
+                    //                    cout << mistakesArray[1] << endl;
+                    //                }
+                    // Question End
                 }
-                // Question End
             }
-        }
             // Easy Version of Hiragana End
             
-            // Over
-            auto questionString = to_string(question - 2);
-            auto correctAnswersString = to_string(correctAnswers);
-            cout << "Thank you for your hard work! You answered a total of " + questionString + " questions, of which " + correctAnswersString + " were correct." << endl;
-            cout << "If you have a good idea, please contact me at Discord: Rinsuki#8715." << endl;
-            return 0;
+            //    // Debug Mode
+            //    cout << mistakesArray[0] << endl;
+            //    cout << "Do you need to see the hiragana that was answered incorrectly? (y/n)" << endl;
+            //    cin >> input;
+            //    if (input == "y") {
+            //        cout << "Here are the hiragana that were answered incorrectly more than once:" << endl;
+            //        vector<string> unskilled;
+            //        for (int i = 0; i < unskilled.size(); i++) {
+            //            cout << unskilled[i] << endl;
+            //
+            //        }
+            //        cout << "\nMore than twice:" << endl;
+            //        vector<string> mistakesArray;
+            //        for (int j = 0; j < mistakesArray.size(); j++) {
+            //            cout << mistakesArray[j] << endl;
+            //
+            //        }
+            //
         }
+        // Over
+        auto questionString = to_string(question - 2);
+        auto correctAnswersString = to_string(correctAnswers);
+        cout << "Thank you for your hard work! You answered a total of " + questionString + " questions, of which " + correctAnswersString + " were correct." << endl;
+        cout << "\nIf you have a good idea, please contact me at Discord: Rinsuki#8715." << endl;
+        return 0;
+    }
+}
