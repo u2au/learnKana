0c1832ddea9ad8f3321c2b3bb02d03c07a48ffa7
Rinsuki <25454024+rinsukiyo@users.noreply.github.com>
2023-01-19 03:01:33 +0800

    Code Simplification
    Co-Authored-By: yiichii <92780051+emovodka@users.noreply.github.com>

--- src/main/java/cc/rinsuki/learnkana/Home.java
+++ src/main/java/cc/rinsuki/learnkana/Home.java
@@ -2,12 +2,10 @@ package cc.rinsuki.learnkana;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Home extends Application
 {
--- src/main/java/cc/rinsuki/learnkana/SceneController.java
+++ src/main/java/cc/rinsuki/learnkana/SceneController.java
@@ -19,9 +19,18 @@ public class SceneController {
 
     // Initialization
     @FXML
-    Label startButton, numOfAnsLabel, kanaLabel, kanaAnsLabel, ansFeedbackLabel, wrongKanaLabel, questionLabel;
+    Label startButton,
+          numOfAnsLabel,
+          kanaLabel,
+          kanaAnsLabel,
+          ansFeedbackLabel,
+          wrongKanaLabel,
+          questionLabel;
 
-    private static int mode, numOfKana, numOfAns = 0, numOfWrongKana = 0;
+    private static int mode,
+                       numOfKana,
+                       numOfAns = 0,
+                       numOfWrongKana = 0;
 
     private static boolean existedKana = false;
 
@@ -29,20 +38,19 @@ public class SceneController {
                          firstKana = 1;
 
     private static String kana,
-                   kanaAns,
-                   inputAns,
-                   lastKana,
-                   lastKanaAns,
-                   feedbackText,
-                   wrongKanaLabelText = "";
+                          kanaAns,
+                          feedbackText,
+                          wrongKanaLabelText = "";
 
     @FXML private TextField input;
-    @FXML private Button nextButton, checkButton;
+    @FXML private Button nextButton,
+                         checkButton;
 
-    String[] wrongKana = new String[71];
-    String[] wrongPronunciation = new String[71];
+    private final String[] wrongKana = new String[71];
+    private final String[] wrongPronunciation = new String[71];
 
-    private String[] kanaAnswer = {
+    // Set pronunciation answers
+    private final String[] kanaAnswer = {
             // a
             "a", "i", "u", "e", "o",
             // ka
@@ -81,7 +89,8 @@ public class SceneController {
     };
     // Set pronunciation answers END
 
-    String[] hiragana = {
+    // Set kana
+    private final String[] hiragana = {
             // a
             "あ", "い", "う", "え", "お",
             // ka
@@ -156,29 +165,20 @@ public class SceneController {
             "パ", "ピ", "プ", "ペ", "ポ"
             // Voiceless and voiced katakana END
     };
+    // Set kana END
 
     // Check if one kana had been added to array before
-    private boolean checkArr(String k)
-    {
-        return (Arrays.asList(wrongKana).contains(k));
-    }
+    private boolean checkArr(String k) { return (!Arrays.asList(wrongKana).contains(k)); }
 
     // Check if kana pronunciation is correct
-    private void checkKana(String kp)
+    private void checkKana()
     {
-//        // If incorrect
-//        if (!kp.equals(kanaAnswer[whichKana]))
-//        {
             // Get kana and check if kana exists
             if (mode == 1 || mode == 3) // Hiragana
             {
                 // If kana not exist
-                if (!checkArr(hiragana[whichKana]))
-                {
+                if (checkArr(hiragana[whichKana]))
                     wrongKana[numOfWrongKana] = hiragana[whichKana];
-                    System.out.println(wrongKana[numOfWrongKana] + " " + hiragana[whichKana]); // DEBUG
-
-                }
 
                 else existedKana = true;
             }
@@ -186,37 +186,25 @@ public class SceneController {
             else // Katakana
             {
                 // If kana not exist
-                if (!checkArr(katakana[whichKana]))
-                {
+                if (checkArr(katakana[whichKana]))
                     wrongKana[numOfWrongKana] = katakana[whichKana];
-                }
 
                 else existedKana = true;
             }
             // Get kana and check if kana exists END
 
-//        }
-
         if (!existedKana)
         {
             wrongPronunciation[numOfWrongKana] = kanaAnswer[whichKana];
-            System.out.println(wrongPronunciation[numOfWrongKana] + " " + kanaAnswer[whichKana]); // DEBUG
 
             // Check if exceed max num of wrong kana
             // Need modification
             if (numOfWrongKana >= 15)
-            {
                 firstKana = 1;
-            }
 
             else
-            {
                 // Save kana and pronunciation information
-                System.out.println(wrongKana[numOfWrongKana] + wrongPronunciation[numOfWrongKana]); // DEBUG
                 wrongKanaLabelText += (wrongKana[numOfWrongKana] + " (" + wrongPronunciation[numOfWrongKana++] + ")\n");
-                System.out.println(wrongKana[numOfWrongKana] + wrongPronunciation[numOfWrongKana]); // DEBUG
-
-            }
         }
 
         existedKana = false; // Restore existing status
@@ -238,10 +226,11 @@ public class SceneController {
                 {
                     feedbackText = "Incorrect. Answer for " + kana + " is " + kanaAnswer[whichKana] + ".";
                     ansFeedbackLabel.setText(feedbackText);
-                    checkKana(input.getText());
+                    checkKana();
                 }
 
-                else {
+                else
+                {
                     input.setText(""); // Clear text field
                     ansFeedbackLabel.setText("Correct.");
                 }
@@ -258,7 +247,6 @@ public class SceneController {
                 ansFeedbackLabel.setText("Please press ENTER to jump to next kana.");
                 nextButton.setDisable(false);
                 checkButton.setDisable(false);
-
                 break;
 
             default:
@@ -279,27 +267,19 @@ public class SceneController {
         else
         {
             kanaAnsLabel.setText(""); // Set as default
-            numOfAnsLabel.setText(("No." + String.valueOf(++numOfAns))); // Numbering of Kana
+            numOfAnsLabel.setText(("No." + (++numOfAns))); // Numbering of Kana
             whichKana = (short)(Math.random() * numOfKana); // Generate random num
             kanaAns = kanaAnswer[whichKana]; // Set answer
 
             if (mode == 1 || mode == 3)
-            {
                 kana = hiragana[whichKana];
-            }
 
-            else
-            {
-                kana = katakana[whichKana];
-            }
+            else kana = katakana[whichKana];
 
             // Display new kana
             kanaLabel.setText(kana);
 
-            System.out.println(mode + " " + numOfKana + " " + kana + " " + kanaAns); // DEBUG
         }
-
-
 
     }
     // Review Kana END
@@ -310,41 +290,6 @@ public class SceneController {
     {
         kanaAnsLabel.setText(kanaAns);
     }
-
-    // Switch between scenes
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    public void switchToReviewScene(ActionEvent event) throws IOException
-    {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Review.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-    public void switchToReportScene(ActionEvent event) throws IOException
-    {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Report.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        firstKana = 1; // Set as default
-        stage.show();
-    }
-
-    public void switchToHomeScene(ActionEvent event) throws IOException
-    {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-    // Switch between scenes END
 
     private void setMode(int m, int n, String mn)
     {
@@ -381,9 +326,49 @@ public class SceneController {
     @FXML
     private void generateReport()
     {
-        wrongKanaLabel.setText(wrongKanaLabelText);
-        System.out.println(wrongKanaLabelText); // DEBUG
-        wrongKanaLabelText = "";
+        if (wrongKanaLabelText.equals(""))
+            wrongKanaLabel.setText("Wow, you answered all kana correctly!");
+
+        else
+        {
+            wrongKanaLabel.setText(wrongKanaLabelText);
+            wrongKanaLabelText = "";
+        }
+
     }
+
+    // Switch between scenes
+    private Stage stage;
+    private Scene scene;
+
+    public void switchToReviewScene(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Review.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void switchToReportScene(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Report.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        firstKana = 1; // Set as default
+        stage.show();
+    }
+
+    public void switchToHomeScene(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Home.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    // Switch between scenes END
 
 }
